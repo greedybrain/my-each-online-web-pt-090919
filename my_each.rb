48f4a5b608b57788c39ba20fc814086f0273b617
Naya  Willis <greedybrain18@gmail.com>
2019-09-23 15:53:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,15 @@
 def my_each(array)
-  i = 0 
+  collection = []
+  i = 0 # counter
   while i < array.length 
-    yield array[i]
-    i += 1
+    collection << yield(array[i])
+    i += 1 
   end
   array
+end
+
+products = []
+
+my_each([1,2,3,4,5]) do |number_to_be_mult_by_3|
+  products << number_to_be_mult_by_3 * 3
 end
